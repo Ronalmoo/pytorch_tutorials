b897add10f8e0e65e145c04a707aeadbe783b1b9
Ronalmoo <ydaniel0826@gmail.com>
2019-07-04 18:39:16 +0900

    # neural networks

--- deep_learning_with_pytorch_a_60_minute_blitz/neural_networks.py
+++ deep_learning_with_pytorch_a_60_minute_blitz/neural_networks.py
@@ -36,6 +36,50 @@ class Net(nn.Module):
             num_features *= s
         return num_features
 
+
 net = Net()
 print(net)
 
+
+params = list(net.parameters())
+print(len(params))
+print(params[0].size()) # conv1's weight
+
+# Let try a random 32X32 input.
+
+input = torch.randn(1, 1, 32, 32)
+out = net(input)
+print(out)
+
+net.zero_grad()
+out.backward(torch.randn(1, 10))
+
+
+# Loss Function
+
+output = net(input)
+print(output)
+target = torch.randn(10)
+print(target)
+target = target.view(1, -1)
+print(target)
+criterion = nn.MSELoss()
+
+loss = criterion(output, target)
+print(loss)
+
+print(loss.grad_fn)  # MSELoss
+print(loss.grad_fn.next_functions[0][0])  # Linear
+print(loss.grad_fn.next_functions[0][0].next_functions[0][0])  # ReLU
+
+
+# Backprop
+net.zero_grad() # zeros the gradient buffers of all parameters
+
+print('conv1.bias.grad before backward')
+print(net.conv1.bias.grad)
+
+loss.backward()
+
+print('conv1.bias.grad after backward')
+print(net.conv1.bias.grad)
